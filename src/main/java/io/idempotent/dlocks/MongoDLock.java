4634b0a6226b1a0a70f8d0b3da1179b9d30b7199
Kevin Jing Qiu <kevin@idempotent.ca>
2020-10-24 01:34:24 -0400

    Create index

--- src/main/java/io/idempotent/dlocks/MongoDLock.java
+++ src/main/java/io/idempotent/dlocks/MongoDLock.java
@@ -4,8 +4,11 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.IndexOptions;
+import com.mongodb.client.model.Indexes;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class MongoDLock implements DLock, AutoCloseable {
     private final MongoClient mongoClient;
@@ -16,6 +19,13 @@ public class MongoDLock implements DLock, AutoCloseable {
         mongoClient = MongoClients.create(mongoConnectionStr);
         db = mongoClient.getDatabase(dbName);
         coll = db.getCollection(collName, MongoDLockDoc.class);
+        ensureIndex();
+    }
+
+    private void ensureIndex() {
+        IndexOptions options = new IndexOptions();
+        options.expireAfter(10L, TimeUnit.MINUTES);
+        coll.createIndex(Indexes.ascending("renewedAt"), options);
     }
 
     public boolean tryAcquire() {
